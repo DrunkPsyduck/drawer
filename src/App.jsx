4ef50ac04445f3d7dda38a207858533e59f8b2e1
Mario <40211718+DrunkPsyduck@users.noreply.github.com>
2023-08-10 11:48:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h3 style={{position: 'relative', zIndex: '2'}}>Drawer by <a href="https:/mariocanales.es" target="_blank"> Mario Canales </a></h3>
+      <h3 style={{position: 'relative', zIndex: '2'}}>Drawer by <a href="https://mariocanales.es" target="_blank"> Mario Canales </a></h3>
       <Draw />
     </>
   )
