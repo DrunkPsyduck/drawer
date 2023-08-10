ea2c6308d525e1c51b95af1e7516a3c4dd2ea6c0
Mario <40211718+DrunkPsyduck@users.noreply.github.com>
2023-08-10 12:08:20 +0200

    Update Draw.jsx

--- src/components/Draw.jsx
+++ src/components/Draw.jsx
@@ -1,29 +1,55 @@
 import { useRef, useEffect } from "react";
 const Draw = () => {
-  const canvasRef = useRef(null);
+const canvasRef = useRef(null);
+  const isDrawingRef = useRef(false);
   const lastPositionRef = useRef(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
 
-    canvas.addEventListener("click", (e) => {
+    const handleMouseDown = (e) => {
+      isDrawingRef.current = true;
+      lastPositionRef.current = {
+        x: e.pageX - canvas.offsetLeft,
+        y: e.pageY - canvas.offsetTop,
+      };
+    };
+
+    const handleMouseMove = (e) => {
+      if (!isDrawingRef.current) return;
+
       ctx.lineWidth = 1.5;
       ctx.lineCap = "round";
       ctx.strokeStyle = "#037A68";
       ctx.globalAlpha = 1.5;
 
-      const { pageX, pageY } = e;
+      const { x, y } = lastPositionRef.current;
 
-      if (lastPositionRef.current) {
-        const { x, y } = lastPositionRef.current;
-        ctx.moveTo(x - canvas.offsetLeft, y - canvas.offsetTop);
-        ctx.lineTo(pageX - canvas.offsetLeft, pageY - canvas.offsetTop);
-        ctx.stroke();
-      }
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+      ctx.stroke();
 
-      lastPositionRef.current = { x: pageX, y: pageY };
-    });
+      lastPositionRef.current = {
+        x: e.pageX - canvas.offsetLeft,
+        y: e.pageY - canvas.offsetTop,
+      };
+    };
+
+    const handleMouseUp = () => {
+      isDrawingRef.current = false;
+    };
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
   }, []);
 
   return (
